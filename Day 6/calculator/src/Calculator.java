13b8806bbb6b64d0373902709b292a22a1e53093
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-03 20:09:22 +0000

    Calculator Complete

--- Day 6/calculator/src/Calculator.java
+++ Day 6/calculator/src/Calculator.java
@@ -1,22 +1,24 @@
-public Calculator {
+public class Calculator {
 	
-	void public add(int i1, int i2){
+	public void add(int i1, int i2){
 		System.out.println(i1 + i2);
 	}
 	
-	void public subtract(int i1, int i2){
+	public void subtract(int i1, int i2){
 		System.out.println(i1 - i2);
 	}
 	
-	void public multiply(int i1, int i2){
+	public void multiply(int i1, int i2){
 		System.out.println(i1 * i2);
 	}
-	void public divide(int i1, int i2){
+	public void divide(int i1, int i2){
 		double d1 = (double)i1;
 		double d2 = (double)i2;
+		System.out.println(d1/d2);
+		
 	}
 	
-	void public modulus(int i1, int i2){
+	public void modulus(int i1, int i2){
 		System.out.println(i1 % i2);
 	}
 	
--- Day 6/calculator/src/CalculatorTest.java
+++ Day 6/calculator/src/CalculatorTest.java
@@ -1,5 +1,23 @@
-public static void main(String[] args){
-	int num1 = 10;
-	int num2 = 5;
-	System.out.println(new Calculator.add(num1,num2));
+class CalculatorTest{
+	public static void main(String[] args) {
+
+	
+		int num1 = 10;
+		int num2 = 5;
+
+		Calculator calc = new Calculator();
+		
+		System.out.print("Add: ");
+		calc.add(num1, num2);
+		System.out.print("Div: ");
+		calc.divide(num1, num2);
+		System.out.print("Sub: ");
+		calc.subtract(num1, num2);
+		System.out.print("Mod: ");
+		calc.modulus(num1, num2);
+		System.out.print("Mul: ");
+		calc.multiply(num1, num2);
+		
+
+	}
 }
