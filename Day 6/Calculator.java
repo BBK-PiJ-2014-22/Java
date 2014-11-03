a71898bdc161da57d46b16137a182a4d87babab6
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-03 19:26:55 +0000

    Calculator

---
+++ Day 6/Calculator.java
@@ -0,0 +1,26 @@
+public Calculator {
+	
+	void public add(int i1, int i2){
+		System.out.println(i1 + i2_;
+	}
+	
+	void public subtract(int i1, int i2){
+		System.out.println(i1 - i2);
+	}
+	
+	void public multiply(int i1, int i2){
+		System.out.println(i1 * i2);
+	}
+	void public divide(int i1, int i2){
+		double d1 = (double)i1;
+		double d2 = (double)i2;
+	}
+	
+	void public modulus(int i1, int i2){
+		System.out.println(i1 % i2);
+	}
+	
+}
+		
+		
+		
