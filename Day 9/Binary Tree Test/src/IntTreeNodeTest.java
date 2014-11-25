769610943227c3460e11088b03d5c2a4b7d65d4a
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-25 22:06:03 +0000

    Tests refactored - remove not working
    Remove is not in fact working. poop.

--- Day 9/Binary Tree Test/src/IntTreeNodeTest.java
+++ Day 9/Binary Tree Test/src/IntTreeNodeTest.java
@@ -5,6 +5,19 @@ public class IntTreeNodeTest {
 
 	public static void main(String[] args) {
 				
+	for (int i = 0; i < 10 ; i++){
+		randomTreeTestGen();
+		System.out.println();
+		System.out.println("********");
+		System.out.println();
+
+	}
+	
+	
+	}
+	
+	public static void randomTreeTestGen(){
+	
 		IntTree tree = new IntTree();
 		
 		Random generator = new Random();
@@ -19,13 +32,34 @@ public class IntTreeNodeTest {
 		System.out.println(stringArray(array));
 		System.out.println();
 		
-		System.out.println("Contains "+array[4] + ": "+tree.contains(array[4]));
+/*		System.out.println("Contains "+array[4] + ": "+tree.contains(array[4]));
 		System.out.println("Contains 101 :"+tree.contains(101));
 		System.out.println("Max: "+tree.getMax());
 		System.out.println("Min: "+tree.getMin());
 		System.out.println(tree.toStringComplete());
 		System.out.println(tree);
-		System.out.println(tree.depth()); 
+		System.out.println(tree.depth());
+*/
+		
+		tree.add(array[3]);
+		tree.add(array[3]);
+		System.out.println(tree);
+		System.out.println();
+		System.out.println("Removing "+ array[3]);
+		tree.remove(array[3]);
+		System.out.println(tree);
+		System.out.println();
+		System.out.println("Removing "+ array[0]);
+		tree.remove(array[0]);
+		System.out.println(tree);
+		System.out.println();
+		System.out.println("Removing 101");
+		tree.remove(101);
+		System.out.println(tree);
+		
+		
+	
+	
 	}
 
 	public static String stringArray(int[] array){
@@ -38,5 +72,6 @@ public class IntTreeNodeTest {
 	}
 	
 }
+
 
 
