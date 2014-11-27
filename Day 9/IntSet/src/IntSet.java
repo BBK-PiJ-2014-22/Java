c631fdbdd06fc2816af4ebb7d9eeb7d6adc57603
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-27 16:52:12 +0000

    IntSet Basics Added

---
+++ Day 9/IntSet/src/IntSet.java
@@ -0,0 +1,14 @@
+
+public interface IntSet {
+
+	void add(int number); //adds a new int to the set; if it is there already, nothing happens.
+	
+	boolean contains(int number); //returns true if the number is in the set, false otherwise.
+	
+	boolean containsVerbose(int number);
+	//returns the same values as the former method, but for every element that is checked prints its value on screen.
+	
+	String toString();
+	//returns a string with the values of the elements in the set separated by commas.
+
+}
---
+++ Day 9/IntSet/src/IntSetTree.java
@@ -0,0 +1,30 @@
+
+public class IntSetTree implements IntSet {
+
+	
+	
+	
+	
+	@Override
+	public void add(int number) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(int number) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVerbose(int number) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public String toString(){
+		return "";
+	}
+}
