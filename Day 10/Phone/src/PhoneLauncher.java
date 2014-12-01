9e5d40a624c4b7ce3ef6462d0c527dcb07c724be
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-12-01 20:11:46 +0000

    CallList Fixed

--- Day 10/Phone/src/PhoneLauncher.java
+++ Day 10/Phone/src/PhoneLauncher.java
@@ -9,25 +9,29 @@ public class PhoneLauncher {
 	
 	public void launch(){
 		MobilePhone phone = new MobilePhone();
-		phone.call("01234 567890");
+
 		phone.call("01234 567899");
 		phone.call("01234 567898");
 		phone.call("01234 567897");
 		phone.call("01234 567896");
-		
-
-		
 		phone.call("01234 567895");
 		phone.call("01234 567894");
 		phone.call("01234 567893");
 		phone.call("01234 567892");
 		phone.call("01234 567891");
+		phone.call("01234 567890");
+		System.out.println(phone.dialled);
+		
+		phone.call("01234 567889");
+	
+		System.out.println(phone.dialled);
+	
+		phone.call("01234 567888");
+		phone.call("01234 567887");
+		phone.call("01234 567886");
 
 		System.out.println(phone.dialled);
 		
-		phone.call("01234 567880");
-	
-		System.out.println(phone.dialled);
 	}
 
 }
