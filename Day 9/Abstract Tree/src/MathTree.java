7597b2ba9c125bfd1d1318ff3f229e7d6ec5a12d
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-12-01 17:39:15 +0000

    MathTree Complete
    Mat

--- Day 9/Abstract Tree/src/MathTree.java
+++ Day 9/Abstract Tree/src/MathTree.java
@@ -11,32 +11,37 @@ public class MathTree {
 	//right = string2
 	
 	
-	public String value;
-	public MathTree right;
-	public MathTree left;
-	public boolean operator;
+	private String value;
+	private MathTree right;
+	private MathTree left;
+	private boolean operator;
 
 	
+	public static MathTree parseMathTree(String formula){
+		return new MathTree(formula);
+	}
 	
+	@Override
+	public String toString(){
+		if (this.left == null && this.right == null){
+			return this.value;
+		}else{
+			return "("+this.left.toString()+this.value+this.right.toString()+")";
+		}
+	}
 	
 
 	public String getValue() {
 		return value;
 	}
 
-
-
 	public MathTree getRight() {
 		return right;
 	}
 
-
-
 	public MathTree getLeft() {
 		return left;
 	}
-
-
 
 	public boolean isOperator() {
 		return operator;
@@ -45,43 +50,65 @@ public class MathTree {
 	
 
 	public MathTree(String formula){
-		
+	
 		int bracketCount = 0;
 		int plusMinusPosition = 0;
 		int multiplyDividePosition = 0;
+		int symbolPosition;
 		String token;
 		
+		
+		//Cuts off the outermost brackets, if present.
+		formula = trimBrackets(formula);
 	
-		for (int i = 1; i < formula.length() ; i++){
-			token = formula.substring(i-1,i);
-			if (token == "("){
-				bracketCount += 1;
-			}else if(token == ")"){
-				bracketCount -= 1;
-			}else if (isOperator(token)){
-				if (bracketCount == 0){
-					if ((token == "+" || token == "-") && plusMinusPosition == 0){
-						plusMinusPosition = i;
-					}else if (multiplyDividePosition == 0){
-						multiplyDividePosition = i;
-					}
+		//Looks through the string to establish first operators in the area of maximal scope
+		//Formulas of length 1 cannot be symbols
+		if (formula.length() > 1){
+			for (int i = 1; i < formula.length() ; i++){
+				token = formula.substring(i-1,i);
+				if (token.equals("(")){
+					bracketCount += 1;
+				}else if(token.equals(")")){
+					bracketCount -= 1;
+				}else if (isOperator(token)){
+					if (bracketCount == 0){
+						if ((token.equals("+")|| token.equals("-")) && plusMinusPosition == 0){
+							plusMinusPosition = i;
+						}else if (multiplyDividePosition == 0){
+							multiplyDividePosition = i;
+						}
+					}		
 				}		
-			}		
+			}
 		}
 		
+		//If a symbol has been found, appoints the left most symbol of maximum scope to the symbol position
+		if (multiplyDividePosition > 0){
+			symbolPosition = multiplyDividePosition;
+		}else if(plusMinusPosition > 0){
+			symbolPosition = plusMinusPosition;
+		}else{
+			symbolPosition = 0;
+		}
 		
-		
-		
+		if (symbolPosition > 0){
+			this.left  = new MathTree(formula.substring(0,symbolPosition-1));
+			this.value = formula.substring(symbolPosition-1, symbolPosition);
+			this.right = new MathTree(formula.substring(symbolPosition,formula.length()));
+		}else{
+			this.value = formula;
+		}
+
 	}
 	
 	
 	
 	//Is Operator
 	private boolean isOperator(String token){
-		if (token == "*" ||
-			token == "/" ||
-			token == "-" ||
-			token == "+"){
+		if (token.equals("*") ||
+			token.equals("/") ||
+			token.equals("-") ||
+			token.equals("+")){
 			return true;
 		}
 		else{
@@ -90,6 +117,12 @@ public class MathTree {
 	}
 	
 	
-	
+	private static String trimBrackets(String str){
+		if (str.charAt(0) == '('){
+			return str.substring(1, str.length());
+		}else{
+			return str;
+		}
+	}
 	
 }
--- Day 9/Abstract Tree/src/MathTreeTest.java
+++ Day 9/Abstract Tree/src/MathTreeTest.java
@@ -3,10 +3,15 @@ public class MathTreeTest {
 
 	public static void main(String[] args) {
 		
+		
+		MathTree test = new MathTree(("2*(2+2*2))+2*2"));
+		System.out.println(test);
+		
+		
 		simpleParseTest("2*2", "2", "*", "2", "Simple Parse Test 1 Failed");
-		simpleParseTest("12/2", "12", "*", "2", "Simple Parse Test 2 Failed");
+		simpleParseTest("12*2", "12", "*", "2", "Simple Parse Test 2 Failed");
 		simpleParseTest("2+12", "2", "+", "12", "Simple Parse Test 3 Failed");
-		simpleParseTest("2*2", "4", "/", "4", "Simple Parse Test 4 Failed");
+		simpleParseTest("4/4", "4", "/", "4", "Simple Parse Test 4 Failed");
 		
 		
 		
@@ -18,19 +23,17 @@ public class MathTreeTest {
 		MathTree result = new MathTree(input);
 		boolean failed = false;
 		
-		
-		
-		if (!result.left.value.equals(left)){
+		if (!result.getLeft().toString().equals(left)){
 			failed = true;
-			System.out.println("Left Failed: "+ result.left.value + "!="+left);
+			System.out.println("Left Failed: "+ result.getLeft().toString() + "!="+left);
 		}
-		if (!result.right.value.equals(right)){
+		if (!result.getRight().toString().equals(right)){
 			failed = true;
-			System.out.println("Right failed"+ result.right.value + "!="+right);
+			System.out.println("Right failed: "+ result.getRight().toString() + "!="+right);
 			
-		}if (!(result.value == operator)){
+		}if (!(result.getValue().equals(operator))){
 			failed = true;
-			System.out.println("Operator failed"+ result.value + "!="+operator);
+			System.out.println("Operator failed: "+ result.getValue() + "!="+operator);
 		}
 		if (failed){
 			System.out.println(message);
