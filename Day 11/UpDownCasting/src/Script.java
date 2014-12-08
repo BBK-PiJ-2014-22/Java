c7a602c0c7d10d8c22a306d5b3fcf96e2568441c
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-12-08 18:26:43 +0000

    Tested with mobile phone

--- Day 11/UpDownCasting/src/Script.java
+++ Day 11/UpDownCasting/src/Script.java
@@ -7,24 +7,29 @@ public class Script {
 	}
 
 	public void launch(){
-		MobilePhone mobile = new SmartPhone("Fairphone");
-		SmartPhone smart = (SmartPhone) mobile;
+		Phone mobile = new SmartPhone("Fairphone");
 		
-			System.out.println("Smart functions:");
-
-			smart.call("00123456789");
-			smart.browseWeb("www.google.com");
-			smart.findPosition("Here");
-			System.out.println();
-			
-			System.out.println("Mobile functions:");
-			System.out.println(smart.displayDialled());
-			System.out.println("");
-			
-			System.out.println("Old functions:");
-			System.out.println(smart.getBrand());	
-			smart.call("0123456789");
-		
+		testPhone(mobile);
 		
 	}
+	
+	public void testPhone(Phone phone){
+		
+		SmartPhone smart = (SmartPhone) phone;
+		
+		System.out.println("Smart functions:");
+		smart.call("00123456789");
+		smart.browseWeb("www.google.com");
+		smart.findPosition("Here");
+		System.out.println();
+		
+		System.out.println("Mobile functions:");
+		System.out.println(smart.displayDialled());
+		System.out.println("");
+		
+		System.out.println("Old functions:");
+		System.out.println(smart.getBrand());	
+		smart.call("0123456789");
+	}
+	
 }
