4fd840ffc8ec519b864bd091071698324d0b2ca4
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-02 18:07:13 +0000

    Folder Rename

---
+++ Day 15/NewExceptions/src/NewExceptions.java
@@ -0,0 +1,19 @@
+
+public class NewExceptions {
+
+	public static void main(String[] args) {
+
+		this.launch();
+		
+	}
+
+	public void launch(){
+		try{
+			
+		}
+	}
+	
+	public void throwChecked(){
+		throw new SomeCheckedException();
+	}
+}
