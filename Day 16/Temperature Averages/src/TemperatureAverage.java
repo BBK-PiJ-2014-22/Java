0635f099aff535424d11ce134c2b934329cbe2e2
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-02 21:03:12 +0000

    Basic approach written for TempA

--- Day 16/Temperature Averages/src/TemperatureAverage.java
+++ Day 16/Temperature Averages/src/TemperatureAverage.java
@@ -1,34 +1,44 @@
 	import java.io.BufferedReader;
-	import java.io.File;
-	import java.io.FileNotFoundException;
-	import java.io.FileReader;
-	import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TemperatureAverage {
 
 	final String DELIMITER = ",";
 	
 
+	//Reader - gets line (String)
 	
-	public int[] splitLine(String filename){
+		//ReadLine(String
+			//Split tokens
+			//Parse tokens as ints
+			//Average and count tokens
+			//returns int[av][num]
+		
+		//Adds to int[][]
+			
+	
 		
 	
-	}
-	
-	public void readLines(String filename){
+	public void launch(String filename){
 			String line;
 			String[] tokens;
+			List temperatureRecords = new ArrayList();
 				
 			BufferedReader reader = null;
 			try{
 				FileReader in = new FileReader(filename);
 				reader = new BufferedReader(in);
 				while (((line = reader.readLine()) != null)){
-					tokens = line.split(DELIMITER);
-					
+					temperatureRecords.add(this.parseLine(line));
 				}
 				
-							
+				this.printRecords(temperatureRecords);
+				
 			}catch (FileNotFoundException ex){
 				System.out.println("No such file '"+filename+"'");
 				
@@ -42,11 +52,27 @@ public class TemperatureAverage {
 					ex.printStackTrace();
 				}
 			}
-			
 		}
 
+
+	private void printRecords(List temperatureRecords) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	
-	
+
+	private int[] parseLine(String line) {
+		int[] result = new int[2];
+		String[] tokens = line.split(DELIMITER);
+		
+		
+		//TODO - Add in a splitter here
+		
+		
+		
+		return result;
+	}
+
 }
+
+
