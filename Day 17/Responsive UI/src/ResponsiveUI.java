e64725ed28eff7b7f3a2d12c7f086a51e81f71ff
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-09 20:07:01 +0000

    ResponsiveUI complete

--- Day 17/Responsive UI/src/ResponsiveUI.java
+++ Day 17/Responsive UI/src/ResponsiveUI.java
@@ -1,23 +1,34 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Queue;
-
 
 public class ResponsiveUI {
 
-	int taskLength;
-	Queue<Integer> finished;
-	
-	
-	
+	ConcurrentLinkedQueue<Integer> finished;
+		
 	public static void main(String[] args) {
 		ResponsiveUI rui = new ResponsiveUI();
 		rui.launch();
 	}
 
 	public void launch(){
+		
+		finished = new ConcurrentLinkedQueue();
+		
+		String display = "";
+		
 		for (int i = 0 ; i < 10 ; i++){
-			taskLength = Integer.parseInt(System.console().readLine());
-			
+			System.out.println("Enter task time for task "+i);
+			int taskLength = Integer.parseInt(System.console().readLine());
+			Task nextTask = new Task(i, taskLength, finished);
+			Thread newThread = new Thread(nextTask);
+			newThread.start();
+							
+			while (!finished.isEmpty()){
+				if (finished.peek() != null)
+					display += finished.poll() + " ";	
+			}
+			System.out.println(display);
+			display = "";
 		}
 	}
-	
 }
