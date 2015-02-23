b001e2141e3a1486649c9ff84f86f91d025f2aba
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-23 19:21:37 +0000

    ExecutorImpl Created
    Does not notify on thread completion yet

--- Day 18/Executor/src/ExecutorImpl.java
+++ Day 18/Executor/src/ExecutorImpl.java
@@ -1,12 +1,43 @@
 import java.util.concurrent.Executor;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 public class ExecutorImpl implements Executor {
 
+	
+	ConcurrentLinkedQueue<Runnable> queue;
+	int threadLimit;
+	
+	public ExecutorImpl(int threadLimit){
+		queue = new ConcurrentLinkedQueue<Runnable>();
+		this.threadLimit = threadLimit;
+		this.threadManager();
+	}
+	
 	@Override
 	public void execute(Runnable command) {
-		// TODO Auto-generated method stub
-
+		queue.add(command);
+		notifyAll();
 	}
-
+	
+	private void threadManager(){
+		while (true){
+			if (checkAddThreads()){
+				Thread t = new Thread(queue.remove());
+				t.start();
+			}else{
+				try{
+					wait();
+				}catch (InterruptedException ex){
+					System.out.println("Debug: Notification Recieved");
+				}finally{
+					System.out.println("Debug: TM Finally Called");
+				}
+			}
+		}
+	}
+	
+	private boolean checkAddThreads(){
+		return Thread.activeCount()<= threadLimit && !queue.isEmpty();
+	}
 }
