f3ef6231bb31888730f8b15a9a8f92935818360f
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-02-23 18:49:33 +0000

    ExecutorImpl file created

---
+++ Day 18/Executor/src/ExecutorImpl.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.Executor;
+
+
+public class ExecutorImpl implements Executor {
+
+	@Override
+	public void execute(Runnable command) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
